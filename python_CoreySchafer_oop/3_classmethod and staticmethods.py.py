fb67ad777a21d743361d82efb42f15db3ffcac5c
dusnazarov.ed@gmail.com <dusnazarov.ed@gmail.com>
2024-05-01 19:39:55 -0500

    commited

--- python_CoreySchafer_oop/3_classmethod and staticmethods.py.py
+++ python_CoreySchafer_oop/3_classmethod and staticmethods.py.py
@@ -37,52 +37,52 @@
 
 
 # #////////////////////////////////////////
-class Employee:
-    num_of_emps = 0
-    raise_amt = 1.04  
+# class Employee:
+#     num_of_emps = 0
+#     raise_amt = 1.04  
       
-    def __init__(self,first,last, pay):
-        self.first = first
-        self.last = last
-        self.pay = pay
-        self.email = first + '.'+ last + '@company.com'        
-        Employee.num_of_emps +=1
+#     def __init__(self,first,last, pay):
+#         self.first = first
+#         self.last = last
+#         self.pay = pay
+#         self.email = first + '.'+ last + '@company.com'        
+#         Employee.num_of_emps +=1
     
-    def fullname(self):
-        return '{} {}'.format(self.first, self.last) 
+#     def fullname(self):
+#         return '{} {}'.format(self.first, self.last) 
     
-    def apply_raise(self):        
-        self.pay = int(self.pay * self.raise_amount)
+#     def apply_raise(self):        
+#         self.pay = int(self.pay * self.raise_amount)
 
-    @classmethod
-    def set_raise_amt(cls, amount):
-        cls.raise_amt = amount
+#     @classmethod
+#     def set_raise_amt(cls, amount):
+#         cls.raise_amt = amount
 
-    @classmethod
-    def from_string(cls, emp_str):
-        # print(emp_str)        
-        first, last, pay = emp_str.split('-')        
-        # print(first, last, pay)
-        print(cls(first, last, pay))        
-        return cls(first, last, pay)    
+#     @classmethod
+#     def from_string(cls, emp_str):
+#         # print(emp_str)        
+#         first, last, pay = emp_str.split('-')        
+#         # print(first, last, pay)
+#         print(cls(first, last, pay))        
+#         return cls(first, last, pay)    
      
 
 
-emp_1 = Employee('Elyor','Dusnazarob', 5000)
-emp_2 = Employee('Test','User', 6000)
+# emp_1 = Employee('Elyor','Dusnazarob', 5000)
+# emp_2 = Employee('Test','User', 6000)
 
-emp_str_1 = 'John-Doe-70000'
-emp_str_2 = 'Steve-Smiths-30000'
-emp_str_3 = 'Jane-Doe-90000'
-
-
-new_emp_1 = Employee.from_string(emp_str_1)
-# new_emp_2 = Employee.from_string(emp_str_2)
-# new_emp_3 = Employee.from_string(emp_str_3)
+# emp_str_1 = 'John-Doe-70000'
+# emp_str_2 = 'Steve-Smiths-30000'
+# emp_str_3 = 'Jane-Doe-90000'
 
 
-# print(new_emp_1.email)
-# print(new_emp_1.pay)
+# new_emp_1 = Employee.from_string(emp_str_1)
+# # new_emp_2 = Employee.from_string(emp_str_2)
+# # new_emp_3 = Employee.from_string(emp_str_3)
+
+
+# # print(new_emp_1.email)
+# # print(new_emp_1.pay)
 
 
 #////////////////////////////////////////
--- python_CoreySchafer_oop/4_Inheritances.py
+++ python_CoreySchafer_oop/4_Inheritances.py
@@ -1,36 +1,43 @@
 # #////////////////////////////////////////
-# class Employee:
+class Employee:
   
-#     raise_amt = 1.04  
+    raise_amt = 1.04  
       
-#     def __init__(self,first,last, pay):
-#         self.first = first
-#         self.last = last
-#         self.pay = pay
-#         self.email = first + '.'+ last + '@company.com'        
+    def __init__(self,first,last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + '.'+ last + '@company.com'        
     
     
-#     def fullname(self):
-#         return '{} {}'.format(self.first, self.last) 
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last) 
     
-#     def apply_raise(self):        
-#         self.pay = int(self.pay * self.raise_amt)  
+    def apply_raise(self):        
+        self.pay = int(self.pay * self.raise_amt)  
 
-# class Developer(Employee):
-#     pass                   
+class Developer(Employee):
+    pass                   
         
         
-# dev_1 = Employee('Corey','Schafer', 5000)
-# dev_2 = Employee('Test','User', 6000)
+# emp_1 = Employee('Corey', 'Schafer', 5000)
+# emp_2 = Employee('Test', 'User', 6000)
+# print(emp_1.email)
+# print(emp_2.email)
 
 # dev_1 = Developer('Corey','Schafer', 5000)
 # dev_2 = Developer('Test','User', 6000)
-
-
 # print(dev_1.email)
 # print(dev_2.email)
 
-# #////////////////////////////////////////
+dev_1 = Developer('Corey','Schafer', 5000)
+dev_2 = Developer('Test','User', 6000)
+print(dev_1.email)
+print(dev_2.email)
+
+
+
+# # #////////////////////////////////////////
 # class Employee:  
 #     raise_amt = 1.04
       
@@ -90,91 +97,90 @@
 # # mgr_1 = Manager('Sue', 'Smith', 9000, [dev_1])
 # mgr_1 = Manager('Sue', 'Smith', 9000)
 
-# # print(dev_1.fullname())
-# # print(dev_1.apply_raise())
-# # print(dev_1.prog_lang)
-# # print(mgr_1.email)
+# print(dev_1.fullname())
+# print(dev_1.apply_raise())
+# print(dev_1.prog_lang)
+# print(mgr_1.email)
 
-# print(mgr_1.add_emp(dev_1))
-# print(mgr_1.employees)
+# # print(mgr_1.add_emp(dev_1))
+# # print(mgr_1.employees)
 
-# print(mgr_1.add_emp(dev_2))
-# print(mgr_1.employees)
+# # print(mgr_1.add_emp(dev_2))
+# # print(mgr_1.employees)
 
-# print(mgr_1.add_emp(dev_3))
-# print(mgr_1.employees)
+# # print(mgr_1.add_emp(dev_3))
+# # print(mgr_1.employees)
 
-# print(mgr_1.add_emp(dev_4))
-# print(mgr_1.employees)
+# # print(mgr_1.add_emp(dev_4))
+# # print(mgr_1.employees)
 
-# mgr_1.remove_emp(dev_3)
-# print(mgr_1.employees)
+# # mgr_1.remove_emp(dev_3)
+# # print(mgr_1.employees)
 
-# mgr_1.print_emps()
-
+# # mgr_1.print_emps()
 
 # #////////////////////////////////////////
-class Employee:  
-    raise_amt = 1.04
+# class Employee:  
+#     raise_amt = 1.04
       
-    def __init__(self, first, last, pay):
-        self.first = first
-        self.last = last
-        self.pay = pay
-        self.email = first + '.'+ last + '@company.com'        
+#     def __init__(self, first, last, pay):
+#         self.first = first
+#         self.last = last
+#         self.pay = pay
+#         self.email = first + '.'+ last + '@company.com'        
     
     
-    def fullname(self):
-        return '{} {}'.format(self.first, self.last) 
+#     def fullname(self):
+#         return '{} {}'.format(self.first, self.last) 
     
-    def apply_raise(self):        
-        self.pay = int(self.pay * self.raise_amt)  
-        return self.pay
+#     def apply_raise(self):        
+#         self.pay = int(self.pay * self.raise_amt)  
+#         return self.pay
        
-class Developer(Employee):
-    raise_amt = 1.04   
+# class Developer(Employee):
+#     raise_amt = 1.04   
     
-    def __init__(self, first, last, pay, prog_lang):
-        super().__init__(first, last, pay)      
-        self.prog_lang = prog_lang
+#     def __init__(self, first, last, pay, prog_lang):
+#         super().__init__(first, last, pay)      
+#         self.prog_lang = prog_lang
 
-class Manager(Employee):   
+# class Manager(Employee):   
     
-    def __init__(self, first, last, pay, employees=None):
-        super().__init__(first, last, pay)
+#     def __init__(self, first, last, pay, employees=None):
+#         super().__init__(first, last, pay)
         
-        if employees is None:
-            self.employees = []
+#         if employees is None:
+#             self.employees = []
                   
-        else:
-            self.employees = employees
+#         else:
+#             self.employees = employees
            
 
-    def add_emp(self, emp):
-        if emp not in self.employees:
-            self.employees.append(emp)
+#     def add_emp(self, emp):
+#         if emp not in self.employees:
+#             self.employees.append(emp)
          
 
-    def remove_emp(self, emp):
-        if emp in self.employees:
-            self.employees.remove(emp)
+#     def remove_emp(self, emp):
+#         if emp in self.employees:
+#             self.employees.remove(emp)
          
-    def print_emps(self):
-        for emp in self.employees:
-            print('-->', emp.fullname())
+#     def print_emps(self):
+#         for emp in self.employees:
+#             print('-->', emp.fullname())
         
-dev_1 = Developer('Elyor','Dusnazarov', 5000, 'python')
-dev_2 = Developer('Test','User', 6000, 'java')
-dev_3 = Developer('Shahnoza','Ulasova', 9000, 'javascript')
-dev_4 = Developer('Mohinur','Qodirova', 10000, 'C++')
+# dev_1 = Developer('Elyor','Dusnazarov', 5000, 'python')
+# dev_2 = Developer('Test','User', 6000, 'java')
+# dev_3 = Developer('Shahnoza','Ulasova', 9000, 'javascript')
+# dev_4 = Developer('Mohinur','Qodirova', 10000, 'C++')
 
-mgr_1 = Manager('Sue', 'Smith', 9000, [dev_1])
+# mgr_1 = Manager('Sue', 'Smith', 9000, [dev_1])
 
-print(isinstance(dev_1, Developer ))
-print(isinstance(mgr_1, Developer ))
+# print(isinstance(dev_1, Developer ))
+# print(isinstance(mgr_1, Developer ))
 
-print(issubclass(Developer, Employee))
-print(issubclass(Manager,Developer))
+# print(issubclass(Developer, Employee))
+# print(issubclass(Manager,Developer))
 
 
 
